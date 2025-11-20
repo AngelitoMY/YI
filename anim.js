2e024791e9454994942eab7b23eea416a59aaedf
AngelitoMY <angelmanuelherediareynoso@gmail.com>
2025-11-20 00:53:20 -0400

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,22 @@
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    var fadeInDuration = 0.1;
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+
+  requestAnimationFrame(updateLyrics); // Llama a la siguiente actualización solo cuando es posible
+}
+
+// Inicia la animación de las letras
+requestAnimationFrame(updateLyrics);
